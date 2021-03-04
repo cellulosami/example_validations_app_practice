e999a0413c8753dcac902537e2e2a833d455ba85
austin shields <ajshields196@gmail.com>
2021-03-03 21:51:53 -0500

    complete index route and add create route

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,16 @@
 class Api::UsersController < ApplicationController
   def index
+    @users = User.all
     render 'index.json.jb'
+  end
+
+  def create
+    @user = User.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email]
+    )
+    @user.save
+    render "show.json.jb"
   end
 end
