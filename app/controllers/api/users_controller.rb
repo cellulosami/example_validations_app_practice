146ee7758d396b63547b927bf206ee99937c9b5f
austin shields <ajshields196@gmail.com>
2021-03-03 22:00:12 -0500

    add update, show, and destroy routes to users

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -16,4 +16,27 @@ class Api::UsersController < ApplicationController
       render json: {errors: @user.errors.full_messages}
     end
   end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.first_name = params[:first_name] || @user.first_name
+    @user.last_name = params[:last_name] || @user.last_name
+    @user.email = params[:email] || @user.email
+    if @user.save
+      render "show.json.jb"
+    else
+      render json: {errors: @user.errors.full_messages}
+    end
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+    @user.destroy
+    render json: {message: "The user has been deleted."}
+  end
 end
